123dbc9735243636a0fd0b17b8305c2d616bd598
tianzi <tianzi@modo.net>
2024-08-26 00:20:22 +0100

    introduce audio_nodes.py and AudioSaverNode node

--- __init__.py
+++ __init__.py
@@ -1,19 +1,21 @@
 from .nodes.web_viewer_node import WebViewerNode
-from .nodes.image_saver_node import ImageSaverNode
-from .nodes.audio_genres_node import AudioGenresNode
+from .nodes.image_nodes import *
+from .nodes.audio_nodes import *
 
 __version__ = "1.0.1"
 
 NODE_CLASS_MAPPINGS = {
     "WebViewerNode": WebViewerNode,
     "ImageSaverNode": ImageSaverNode,
-    "AudioGenresNode": AudioGenresNode
+    "AudioGenresNode": AudioGenresNode,
+    "AudioSaverNode": AudioSaverNode
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "WebViewerNode": "Web Viewer by vrch.io",
     "ImageSaverNode": "Image Saver by vrch.io",
-    "AudioGenresNode": "Get Audio Genres by vrch.io"
+    "AudioSaverNode": "Audio Saver by vrch.io",
+    "AudioGenresNode": "Get Audio Genres by vrch.io",
 }
 
 # WEB_DIRECTORY is the comfyui nodes directory that ComfyUI will link and auto-load.
