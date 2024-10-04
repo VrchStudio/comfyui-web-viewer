ec54dc24fbdb73d181b04b3e02fc9da72dc9e340
tianzi <tianzi@vrch.io>
2024-10-04 17:50:25 +0100

    introduce VrchNodeUtils helper class and update VrchNodeUtils node

--- nodes/osc_control_nodes.py
+++ nodes/osc_control_nodes.py
@@ -2,8 +2,11 @@ from pythonosc import dispatcher, osc_server
 import threading
 import socket
 
-class VrchXYOSCControlNode:
+# Define the category for organizational purposes
+CATEGORY = "vrch.io/control/osc"
 
+class VrchNodeUtils:
+    
     @staticmethod
     def get_default_ip_address():
         try:
@@ -14,6 +17,12 @@ class VrchXYOSCControlNode:
             return local_ip
         except Exception:
             return "127.0.0.1"
+        
+    @staticmethod
+    def remap(value, out_min, out_max):
+        return out_min + (value * (out_max - out_min))
+
+class VrchXYOSCControlNode:
 
     def __init__(self):
         self.x, self.y = 0.0, 0.0
@@ -26,7 +35,7 @@ class VrchXYOSCControlNode:
     def INPUT_TYPES(cls):
         return {
             "required": {
-                "ip": ("STRING", {"multiline": False, "default": VrchXYOSCControlNode.get_default_ip_address()}),
+                "server_ip": ("STRING", {"multiline": False, "default": VrchNodeUtils.get_default_ip_address()}),
                 "port": ("INT", {"default": 8000}),
                 "path": ("STRING", {"default": "/xy"}),
                 "x_output_min": ("INT", {"default": 0, "max": 9999, "min": -9999}),
@@ -37,33 +46,30 @@ class VrchXYOSCControlNode:
             }
         }
 
-    RETURN_TYPES = ("INT", "INT")
-    RETURN_NAMES = ("X", "Y")
+    RETURN_TYPES = ("INT", "INT", "FLOAT", "FLOAT")
+    RETURN_NAMES = ("X", "Y", "X_RAW", "Y_RAW")
     FUNCTION = "load_xy_osc"
-    CATEGORY = "vrch.io/control"
+    CATEGORY = CATEGORY
 
     @classmethod
     def IS_CHANGED(cls, **kwargs):
         return float("NaN")
 
-    def remap(self, value, out_min, out_max):
-        return out_min + (value * (out_max - out_min))
-
-    def load_xy_osc(self, ip, port, path, x_output_min, x_output_max, y_output_min, y_output_max, debug):
+    def load_xy_osc(self, server_ip, port, path, x_output_min, x_output_max, y_output_min, y_output_max, debug):
         
         if x_output_min > x_output_max or y_output_min > y_output_max:
             raise ValueError("Output min value cannot be greater than max value.")
         
         self.shut_down_existing()
         if debug:
-            print(f"Loading OSC server with IP: {ip}, Port: {port}, Path: {path + '/*'}")
+            print(f"Loading OSC server with IP: {server_ip}, Port: {port}, Path: {path + '/*'}")
 
         if not self.initialized or not self.server_thread.is_alive():
-            self.setup_osc_server(ip, port, path + '/*', debug)
+            self.setup_osc_server(server_ip, port, path + '/*', debug)
 
-        x_mapped = int(self.remap(float(self.x), float(x_output_min), float(x_output_max)))
-        y_mapped = int(self.remap(float(self.y), float(y_output_min), float(y_output_max)))
-        return x_mapped, y_mapped
+        x_mapped = int(VrchNodeUtils.remap(float(self.x), float(x_output_min), float(x_output_max)))
+        y_mapped = int(VrchNodeUtils.remap(float(self.y), float(y_output_min), float(y_output_max)))
+        return x_mapped, y_mapped, self.x, self.y
 
     def handle_osc_message(self, address, args, value):
         debug = args[0].get("debug", False) if isinstance(args[0], dict) else False
