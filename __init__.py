9fab5103b6fee2c887924a43321dab9ea4e8fac7
tianzi <tianzi@modo.net>
2024-11-24 04:28:49 +0000

    Bump version: 1.0.13 → 1.0.14

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ from .nodes.text_nodes import *
 from .nodes.key_control_nodes import *
 from .nodes.osc_control_nodes import *
 
-__version__ = "1.0.13"
+__version__ = "1.0.14"
 
 NODE_CLASS_MAPPINGS = {
     "VrchAnyOSCControlNode": VrchAnyOSCControlNode,
